f153cd66d4298c7192cf505b98cf0c53478f8b90
Jackson <jackson.hardaker@gmail.com>
2018-09-25 21:20:19 -0400

    Add morocco map

--- public/class-mapael-waypointer-public.php
+++ public/class-mapael-waypointer-public.php
@@ -134,11 +134,11 @@ class Mapael_Waypointer_Public {
 		wp_enqueue_script( 'mapael-world_countries', 'https://cdnjs.cloudflare.com/ajax/libs/jquery-mapael/2.1.0/js/maps/world_countries.min.js', array( 'mapael' ), null, true );
 		wp_enqueue_script( 'mapael-world_countries_mercator', 'https://cdnjs.cloudflare.com/ajax/libs/jquery-mapael/2.1.0/js/maps/world_countries_mercator.min.js', array( 'mapael' ), null, true );
 		wp_enqueue_script( 'mapael-world_countries_miller', 'https://cdnjs.cloudflare.com/ajax/libs/jquery-mapael/2.1.0/js/maps/world_countries_miller.min.js', array( 'mapael' ), null, true );
-		wp_enqueue_script( 'mapael-world_countries_miller', 'https://cdnjs.cloudflare.com/ajax/libs/jquery-mapael/2.1.0/js/maps/world_countries_miller.min.js', array( 'mapael' ), null, true );
 		wp_enqueue_script( 'sri-lanka-map', plugin_dir_url( __FILE__ ) . 'js/sri_lanka.min.js', array( 'mapael' ), null, true );
 		wp_enqueue_script( 'croatia-map', plugin_dir_url( __FILE__ ) . 'js/croatia.min.js', array( 'mapael' ), null, true );
 		wp_enqueue_script( 'japan-map', plugin_dir_url( __FILE__ ) . 'js/japan.min.js', array( 'mapael' ), null, true );
 		wp_enqueue_script( 'thailand-map', plugin_dir_url( __FILE__ ) . 'js/thailand.min.js', array( 'mapael' ), null, true );
+		wp_enqueue_script( 'morocco-map', plugin_dir_url(__FILE__) . 'js/morocco.min.js', array( 'mapael' ), null, true );
 		wp_enqueue_script( $this->plugin_name, plugin_dir_url( __FILE__ ) . 'js/mapael-waypointer-public.min.js', array( 'jquery', 'mapael', 'waypoints' ), null, true );
 	}
 	
